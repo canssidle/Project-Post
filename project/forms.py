4b9cac1e7233186794009ace11bf9cfc7e56c709
canssidle <canssidlewairimu@gmail.com>
2019-07-02 16:48:20 +0300

    creating the forms

--- project/forms.py
+++ project/forms.py
@@ -1,16 +1,16 @@
-# from django import forms
-# from .models import Project, Review, Profile
-# class UploadSiteForm(forms.ModelForm):
-#     class Meta:
-#         model = Project
-#         exclude = ['user','pub_date']
+from django import forms
+from .models import Project, Review, Profile
+class UploadSiteForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        exclude = ['user','pub_date']
 
-# class ReviewForm(forms.ModelForm):
-#     class Meta:
-#         model = Review
-#         exclude = ['average','project', 'user']
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        exclude = ['average','project', 'user']
 
-# class UpdateProfile(forms.ModelForm):
-#     class Meta:
-#         model = Profile
-#         exclude = ['user']
+class UpdateProfile(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
