84d86d2d38ccfdba0f0975b5a0c2185557f89b69
canssidle <canssidlewairimu@gmail.com>
2019-07-02 16:47:29 +0300

    creating the model class

--- project/models.py
+++ project/models.py
@@ -1,18 +1,18 @@
-# from django.db import models
-# from django.contrib.auth.models import User
+from django.db import models
+from django.contrib.auth.models import User
 
-# # Create your models here.
+# Create your models here.
 
-# class Project(models.Model):
-#     title = models.CharField(max_length = 60)
-#     description = models.TextField()
-#     image = models.ImageField(upload_to='images/')
-#     url = models.CharField(max_length = 60)
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     pub_date = models.DateTimeField(auto_now_add=True)
+class Project(models.Model):
+    title = models.CharField(max_length = 60)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    url = models.CharField(max_length = 60)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True)
 
-#     def __str__(self):
-#         return self.title
+    def __str__(self):
+        return self.title
 
 # class Profile(models.Model):
 #     profile_pic = models.ImageField(upload_to='images/', default='/images/default.jpg')
