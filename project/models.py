8438597146b39110dfec580ad2fc41bba92506f3
canssidle <canssidlewairimu@gmail.com>
2019-07-02 16:48:01 +0300

    creating the review class

--- project/models.py
+++ project/models.py
@@ -23,13 +23,13 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
-# class Review(models.Model):
-#     design = models.IntegerField(default=0)
-#     usability = models.IntegerField(default=0)
-#     content = models.IntegerField(default=0)
-#     average = models.IntegerField(default=0)
-#     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+class Review(models.Model):
+    design = models.IntegerField(default=0)
+    usability = models.IntegerField(default=0)
+    content = models.IntegerField(default=0)
+    average = models.IntegerField(default=0)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-#     def __str__(self):
-#         return self.project.title
+    def __str__(self):
+        return self.project.title
