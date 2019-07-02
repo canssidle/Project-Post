6ca185eda024743567f483f14014553bb24a7c27
canssidle <canssidlewairimu@gmail.com>
2019-07-02 16:46:12 +0300

    testing the profile class

--- project/tests.py
+++ project/tests.py
@@ -13,15 +13,15 @@ class ProjectTestClass(TestCase):
     def test_instance(self):
         self.assertTrue(isinstance(self.project,Project))
 
-# class ProfileTestClass(TestCase):
-#     # Set up method
-#     def setUp(self):
-#         self.user = User.objects.create_user(username='testuser', password='12345')
-#         self.profile = Profile(profile_pic='images/pic', bio='I love cheese', contacts='+254712345678', user = self.user)
+class ProfileTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.profile = Profile(profile_pic='images/pic', bio='I love cheese', contacts='+254712345678', user = self.user)
 
-#     #Testing instance
-#     def test_instance(self):
-#         self.assertTrue(isinstance(self.profile,Profile))
+    #Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile,Profile))
 
 # class ReviewTestClass(TestCase):
 #     # Set up method
