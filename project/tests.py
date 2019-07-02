ab0cb0465136b7aeb25d0f0d7a0e090e1d3f0d04
canssidle <canssidlewairimu@gmail.com>
2019-07-02 16:46:32 +0300

    testing the review class

--- project/tests.py
+++ project/tests.py
@@ -23,15 +23,15 @@ class ProfileTestClass(TestCase):
     def test_instance(self):
         self.assertTrue(isinstance(self.profile,Profile))
 
-# class ReviewTestClass(TestCase):
-#     # Set up method
-#     def setUp(self):
-#         self.user = User.objects.create_user(username='testuser', password='12345')
-#         self.profile = Profile(profile_pic='images/pic', bio='I love cheese', contacts='+254712345678', user = self.user)
-#         self.profile.save()
-#         self.project = Project(id=1, image = 'path/to/image',title='test',description='test caption',url='path/to/project',user=self.user)
-#         self.project.save()
-#         self.review = Review(id=1,project=self.project,design=10,usability=10,content=10,average=7,user=self.user)
+class ReviewTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.profile = Profile(profile_pic='images/pic', bio='I love cheese', contacts='+254712345678', user = self.user)
+        self.profile.save()
+        self.project = Project(id=1, image = 'path/to/image',title='test',description='test caption',url='path/to/project',user=self.user)
+        self.project.save()
+        self.review = Review(id=1,project=self.project,design=10,usability=10,content=10,average=7,user=self.user)
 
 #     #Testing instance
 #     def test_instance(self):
